0d6ae83a570604a0475a8f74fe88f41a5233bc17
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-04-25 17:16:21 -0400

    Contador_de_Usuarios

--- TUho/static/js/Modals.js
+++ TUho/static/js/Modals.js
@@ -1,3 +1,12 @@
+var Inicio = document.getElementById('Inicio')
+Inicio.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  
+})
+
 var exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
@@ -8,7 +17,7 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
 })
 
 var eliminateUserModal = document.getElementById('eliminateUserModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
+eliminateUserModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget
   // Extract info from data-bs-* attributes
