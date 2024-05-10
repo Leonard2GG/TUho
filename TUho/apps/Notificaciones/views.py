03b730ba081e6e03c9eefe0c5f174a8486e9fb20
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-05-10 17:39:04 -0400

    Creando Modelo Notificacion

--- TUho/apps/Notificaciones/views.py
+++ TUho/apps/Notificaciones/views.py
@@ -4,8 +4,10 @@ from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from apps.Usuarios.models import Usuario
+from .models import Notificacion
 
 # Bandeja de entrada
 def Bandeja(request:HttpRequest) -> HttpResponse:
-
-    return render(request,"Notificaciones/Bandeja.html")
+    usuario = request.user
+    notificaciones = Notificacion.objects.filter(para=usuario)
+    return render(request,"Notificaciones/Bandeja.html", {'notificaciones': notificaciones})
