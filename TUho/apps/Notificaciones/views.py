be34722f6f661a68e263ecde3dbb390ce11c38ca
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-05-10 17:22:00 -0400

    Creando Interfaz Bandeja

--- TUho/apps/Notificaciones/views.py
+++ TUho/apps/Notificaciones/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from apps.Usuarios.models import Usuario
 
-# Create your views here.
+# Bandeja de entrada
+def Bandeja(request:HttpRequest) -> HttpResponse:
+
+    return render(request,"Notificaciones/Bandeja.html")
