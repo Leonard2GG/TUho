a334d5478b77e11f449a484c5b78567897df43ec
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-05-18 19:40:02 -0400

    ya puse los templates y los statics en sus apps

--- TUho/apps/Usuarios/views.py
+++ TUho/apps/Usuarios/views.py
@@ -33,7 +33,7 @@ def Login(request:HttpRequest):
         try:
             user_to_auth = Usuario.objects.get(username=username)
         except Usuario.DoesNotExist:
-            return render(request, "Usuario/Login.html", {'response': 'incorrecto', 'message': 'Fallo en la autentificación'})
+            return render(request, "Usuarios/Login.html", {'response': 'incorrecto', 'message': 'Fallo en la autentificación'})
         user = authenticate(request, username=user_to_auth.username, password=password)
         if user is not None:
             login(request, user)
@@ -48,14 +48,14 @@ def Login(request:HttpRequest):
                 return redirect("Administracion")
             return redirect("Inicio")
         else:
-            return render(request, "Usuario/Login.html", {'response': 'incorrecto', 'message': 'Fallo en la autentificación'})
-    return render(request, "Usuario/Login.html")
+            return render(request, "Usuarios/Login.html", {'response': 'incorrecto', 'message': 'Fallo en la autentificación'})
+    return render(request, "Usuarios/Login.html")
 
 # Register
 def Registrar(request:HttpRequest):
     if request.POST:
         if Usuario.objects.filter(username=request.POST['username']).exists():
-            return render(request, "Usuario/Registrar.html", {'response': 'incorrecto', 'messages': ['El usuario ya existe']})
+            return render(request, "Usuarios/Registrar.html", {'response': 'incorrecto', 'messages': ['El usuario ya existe']})
 
         usuario = Usuario()
         username = request.POST['username']
@@ -64,7 +64,7 @@ def Registrar(request:HttpRequest):
         password2 = request.POST['password2']
 
         if password and password2 and password != password2:
-            return render(request, "Usuario/Registrar.html", {'response': 'incorrecto', 'messages': ['Las contraseñas deben coincidir']})
+            return render(request, "Usuarios/Registrar.html", {'response': 'incorrecto', 'messages': ['Las contraseñas deben coincidir']})
 
         usuario.username = username
         usuario.email = email
@@ -80,27 +80,27 @@ def Registrar(request:HttpRequest):
             mensajes = []
             for err in e:
                 mensajes.append(f"{err}")
-            return render(request, "Usuario/Registrar.html", {'response': 'incorrecto', 'messages': mensajes})
+            return render(request, "Usuarios/Registrar.html", {'response': 'incorrecto', 'messages': mensajes})
 
-    return render(request, "Usuario/Registrar.html")
+    return render(request, "Usuarios/Registrar.html")
 
 # Restablecer Contraseña
 class RestablecerContraseña(auth_views.PasswordResetView):
-    template_name = "Usuario/Restablecer Contraseña.html"
+    template_name = "Usuarios/Restablecer Contraseña.html"
     form_class = CustomPasswordResetForm
 
 # Cambiar Contraseña
 class CambiarContraseña(auth_views.PasswordResetConfirmView):
     form_class = ChangePasswordForm
-    template_name = "Usuario/Cambiar Contraseña.html"
+    template_name = "Usuarios/Cambiar Contraseña.html"
 
 # Confirmacion del Restablecer Contraseña  
 def RestablecerContraseñaConfirmado(request):
-    return render(request,"Usuario/Restablecer Contraseña Confirmado.html")
+    return render(request,"Usuarios/Restablecer Contraseña Confirmado.html")
 
 # Confirmación del Cambiar Contraseña
 def CambiarContraseñaConfirmado(request):
-    return render(request,"Usuario/Cambiar Contraseña Confirmado.html")
+    return render(request,"Usuarios/Cambiar Contraseña Confirmado.html")
 
 # Cerrar Sesión
 @login_required
@@ -124,7 +124,7 @@ def ActualizarInf(request:HttpRequest):
         usuario.save()
         return redirect("InfoPersonal")
     form = InformacionPersonal()
-    return render(request,"Usuario/Actualizar Informacion Personal.html",{"form":form})
+    return render(request,"Usuarios/Actualizar Informacion Personal.html",{"form":form})
     
 
 
