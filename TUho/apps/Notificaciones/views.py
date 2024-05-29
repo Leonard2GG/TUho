69d1851ed123f60c99763d308218776fc87cb43f
Arlan <arlan010105@gmail.com>
2024-05-29 10:49:17 -0400

    Context Processor

--- TUho/apps/Notificaciones/views.py
+++ TUho/apps/Notificaciones/views.py
@@ -5,9 +5,12 @@ from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from apps.Usuarios.models import Usuario
 from .models import Notificacion
+from django.contrib.auth.decorators import login_required
 
 # Bandeja de entrada
+@login_required
 def Bandeja(request:HttpRequest) -> HttpResponse:
+    print(request.user)
     usuario = request.user
     notificaciones = Notificacion.objects.filter(para=usuario)
     return render(request,"Notificaciones/Bandeja.html", {'notificaciones': notificaciones})
