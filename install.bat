d45dcffabc255c85552629ae7ce692cd3e8205f2
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-06-03 15:16:13 -0400

    Arreglando el install

--- install.bat
+++ install.bat
@@ -1,4 +1,8 @@
+pip install virtualenv
+python -m virtualenv TUho_Env
 call TUho_Env/Scripts/activate.bat
+pip install django
+pip install djangorestframework
 cd TUho
 python manage.py makemigrations
 python manage.py migrate
