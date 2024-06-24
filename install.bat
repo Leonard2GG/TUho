40d4dea98dc1ea601cbe1f310263edc262b7a2dd
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-06-24 18:47:54 -0400

    Terminaciones ok

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 pip install virtualenv
 python -m virtualenv TUho_Env
 call TUho_Env/Scripts/activate.bat
-python -m pip install -r requirements.txt
 cd TUho
+python -m pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py install
