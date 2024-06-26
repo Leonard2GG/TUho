28c3388676c5e27f86e650522dc36b2efeee5ea7
Leonardo Gonzalez <kiri05062001@gmail.com>
2024-06-25 20:12:33 -0400

    Terminando las Notificaciones ok

--- TUho/notificaciones/views.py
+++ TUho/notificaciones/views.py
@@ -11,7 +11,6 @@ from django.core.paginator import Paginator
 # Bandeja de entrada
 @login_required
 def Bandeja(request:HttpRequest) -> HttpResponse:
-    print(request.user)
     usuario = request.user
     notificaciones = Notificacion.objects.filter(para=usuario)
     paginator = Paginator(notificaciones,10)
@@ -20,10 +19,29 @@ def Bandeja(request:HttpRequest) -> HttpResponse:
     return render(request,"Notificaciones/Bandeja.html", {'page_obj':page_obj})
 
 def BandejaAdmin(request:HttpRequest) -> HttpResponse:
-    print(request.user)
     usuario = request.user
     notificaciones = Notificacion.objects.filter(para=usuario)
     paginator = Paginator(notificaciones,10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(request,"Notificaciones/Bandeja Admin.html", {'page_obj':page_obj})
+
+def BorrarNotificaciones(request:HttpRequest) -> HttpResponse:
+    if request.POST:
+        lista_elementos = request.POST.getlist('selected-item')
+        for e in lista_elementos:
+            Notificacion.objects.get(pk=e).delete()
+    
+    next_url = request.GET.get('next', 'Bandeja')
+    return redirect(next_url)
+
+def VisualizarNotificaciones(request:HttpRequest) -> HttpResponse:
+    if request.POST:
+        lista_elementos = request.POST.getlist('selected-item')
+        for e in lista_elementos:
+            notificacion = Notificacion.objects.get(pk=e)
+            notificacion.visto = True
+            notificacion.save()
+    
+    next_url = request.GET.get('next', 'Bandeja')
+    return redirect(next_url)
